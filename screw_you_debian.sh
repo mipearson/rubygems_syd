f69506f1473f094e1bb5cc7127a8020fc0f51c68
Michael Pearson <mipearson@gmail.com>
2010-06-28 15:24:51 +1000

    Add screw_you_debian.sh

---
+++ screw_you_debian.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# screw_you_debian.sh: install a homedir copy of RubyGems if not present
+# and set up relevant environment variables. If already present, just
+# set up the env variables. Run this either from your .profile or
+# from the command line as:
+#
+# . ./screw_you_debian.sh
+
+set -e
+PWD=`pwd`
+cd $HOME
+if [ ! -d gems ]; then
+    curl http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz | tar -xzf -
+    cd rubygems-1.3.7
+    ruby setup.rb --prefix=$HOME/gems
+    ln -sf $HOME/gems/bin/gem1.8 $HOME/gems/bin/gem
+fi
+
+export RUBYLIB=$HOME/gems/lib
+export GEM_HOME=$HOME/gems
