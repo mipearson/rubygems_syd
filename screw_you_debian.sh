1c9cd7fba7c860c560e9ce8e929c1ca177500cde
Michael Pearson <mipearson@gmail.com>
2010-07-09 15:28:46 +1000

    only cd if we have to

--- screw_you_debian.sh
+++ screw_you_debian.sh
@@ -7,16 +7,17 @@
 #
 # . ./screw_you_debian.sh
 
-PWD=`pwd`
 cd $HOME
 if [ ! -d gems ]; then
+    PWD=`pwd`
     curl http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz | tar -xzf -
     cd rubygems-1.3.7
     ruby setup.rb --prefix=$HOME/gems
     ln -sf $HOME/gems/bin/gem1.8 $HOME/gems/bin/gem
+    rm -rf rubygems-1.3.7
+    cd $PWD
 fi
 
 export PATH=$HOME/gems/bin:$PATH
 export RUBYLIB=$HOME/gems/lib
 export GEM_HOME=$HOME/gems
-cd $PWD
