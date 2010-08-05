20a2907b5e0153db6189047fbe0dff1c339f7baa
Michael Pearson <mipearson@gmail.com>
2010-08-05 15:01:45 +1000

    Remove bootstrapper (as it doesn't work well with bundler) and tighten up syd.sh

--- screw_you_debian.sh
+++ screw_you_debian.sh
@@ -7,10 +7,17 @@
 #
 # . ./screw_you_debian.sh
 
-if [ ! -d gems ]; then
+set -e 
+
+if [ ! -d $HOME/gems ]; then
+    if [ -f $HOME/gems ]; then
+        echo $HOME/gems already exists but is not a directory. Aborting to avoid overwriting it.
+        exit 1
+    fi
+
     PWD=`pwd`
     cd $HOME
-    curl http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz | tar -xzf -
+    wget -O - http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz | tar -xzf -
     cd rubygems-1.3.7
     ruby setup.rb --prefix=$HOME/gems
     ln -sf $HOME/gems/bin/gem1.8 $HOME/gems/bin/gem
@@ -20,5 +27,5 @@ if [ ! -d gems ]; then
 fi
 
 export PATH=$HOME/gems/bin:$PATH
-export RUBYLIB=$HOME/gems/lib
+export RUBYLIB=$HOME/gems/lib:$RUBYLIB
 export GEM_HOME=$HOME/gems
